88f1c940ac891f658f02a96a3b955a9160e38c67
Grisha <guruwolf8@gmail.com>
2023-08-26 00:28:26 +0300

    Creating project

---
+++ src/main/java/com/pro/sky/MockLesson/MockLessonApplication.java
@@ -0,0 +1,13 @@
+package com.pro.sky.MockLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/sky/MockLesson/MockLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.MockLesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
